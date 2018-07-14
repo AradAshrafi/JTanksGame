0afeacdd0b1b343a54ca8cb9bc90da80b34fe92b
Mohamd97Mj <mohamadmojahed604@gmail.com>
2018-07-14 16:33:49 +0430

    some classes deleted

--- src/game/gameObjects/PlayerCannonBullet.java
+++
@@ -1,7 +0,0 @@
-package game.gameObjects;
-
-public class PlayerCannonBullet extends CannonBullet {
-    public PlayerCannonBullet(int locX, int locY, int aimPositionX, int aimPositionY, int speed){
-        super(locX, locY, aimPositionX, aimPositionY, speed);
-    }
-}
